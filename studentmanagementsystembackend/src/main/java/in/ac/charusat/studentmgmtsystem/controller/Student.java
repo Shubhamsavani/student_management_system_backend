d78312c7214aeca384c81f47a924a591e3fc7077
Shubhamsavani <20it126@charusat.edu.in>
2022-09-14 13:54:04 +0530

    Added Student Entity, Student Repository

--- studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/Student.java
+++ studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/Student.java
@@ -1,6 +1,12 @@
-package in.ac.charusat.studentmgmtsystem.controller;
+package in.ac.charusat.studentmgmtsystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
     private Integer id;
     private String name;
     private String address;
@@ -11,6 +17,10 @@ public class Student {
         this.address = address;
     }
 
+    public Student() {
+
+    }
+    
     public Integer getId() {
         return id;
     }
